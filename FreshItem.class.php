653784b58abfd8363dd48805654178d44e2419a3
Gianni <GianniB77@users.noreply.github.com>
2022-10-28 16:20:42 +0200

    feat: add Payable and Invoice Class

--- FreshItem.class.php
+++ FreshItem.class.php
@@ -4,10 +4,12 @@
 class FreshItem extends Item {
 
     private $bestBeforeDate;
+    private $taxPercentage;
 
     function __construct($name, $price, $weight, $bestBeforeDate) {
         parent::__construct($name, $price, $weight);
         $this->bestBeforeDate = $bestBeforeDate;
+        $this->taxPercentage = 1000 - 10*(round($weight / 1000));
     }
 
     public function getBestBeforeDate() {
--- Ticket.class.php
+++ Ticket.class.php
@@ -1,15 +1,17 @@
 <?php
 
 
-class Ticket {
+class Ticket extends payable {
 
     private $reference;
     private $prix;
+    private $taxPercentage = 2500;
 
     public function __construct($reference, $prix)
     {
         $this->reference = $reference;
         $this->prix = $prix;
+        $this->taxPercentage = 2500;
     }
 
     function setRef($reference) {
@@ -29,7 +31,18 @@ class Ticket {
     }
 
     public function __toString() {
-        return $this->reference;
-      }
+        return $this->reference . ", " . number_format($this->prix/100, 2) . " €";
+    }
 
+    public function label() {
+        return "Vous allez payé un ticket à " . number_format($this->prix/100, 2) . "€<br>";
+    }
+
+    public function cost() {
+        return "Le coût de ce qui doit être payé en centimes est " . $this->prix . " centimes.<br>";
+    }
+
+    public function taxRatePerTenThousand() {
+        return "La proportion du coût qui est de la taxe est " . $this->taxPercentage . " pour dix-milles.<br>";
+    }
 }
